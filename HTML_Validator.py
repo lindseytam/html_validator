cf84257d7f4c5a7047df3635269385e4c5e52ac3
lindseytam <42879918+lindseytam@users.noreply.github.com>
2020-02-10 22:23:42 -0800

    Update HTML_Validator.py

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -58,9 +58,7 @@ def _extract_tags(html):
         return tags
 
     if html.count(">") != html.count("<"):
-        with pytest.raises(ValueError):
-    
-            raise ValueError('found < without matching >')
+        raise ValueError('found < without matching >')
 
 
     # find first instance of open angle bracket
