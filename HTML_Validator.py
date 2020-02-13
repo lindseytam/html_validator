1e62285e6fe3f3618e8e2d11f91e311ba1da74ba
lindseytam <42879918+lindseytam@users.noreply.github.com>
2020-02-12 19:46:39 -0800

    Update HTML_Validator.py

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,5 +1,24 @@
 import pytest
 
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
 def validate_html(html):
     '''
     This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
